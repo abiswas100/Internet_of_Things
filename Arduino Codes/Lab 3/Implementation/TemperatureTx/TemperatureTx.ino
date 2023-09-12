e4589338aea3189b17fc61e1127d0caaa2f3fb9b
Avhishek <abiswas3@huskers.unl.edu>
2023-09-12 00:16:41 -0500

    Changed the Timer to T4_T5

--- Arduino Codes/Lab 3/Implementation/TemperatureTx/TemperatureTx.ino
+++ Arduino Codes/Lab 3/Implementation/TemperatureTx/TemperatureTx.ino
@@ -1,11 +1,12 @@
 #define CPU_HZ 48000000
 #define TIMER_PRESCALER_DIV 1024
 #define WINDOW_SIZE 5  // Size of the sliding window
-
-#include <TemperatureZero.h>
 #include <SPI.h>
 //Radio Head Library:
 #include <RH_RF95.h>
+
+#include <TemperatureZero.h>
+
 
 TemperatureZero TempZero = TemperatureZero();
 volatile bool canReadTemp = false;
@@ -16,11 +17,13 @@ int tempCount = 0;  // Count of temperatures added to the buffer
 int reportCount = 0;  // Count of readings before reporting
 float avgTemperature = 0;  // To hold the average temperature
 bool newAvgAvailable = false;  // Flag to indicate a new average is available
+// We need to provide the RFM95 module's chip select and interrupt pins to the, rf95 instance below.On the SparkFun ProRF those pins are 12 and 6 respectively.
 RH_RF95 rf95(12, 6);
 int LED = 13; //Status LED is on pin 13
 int packetCounter = 0; //Counts the number of packets sent
 long timeSinceLastPacket = 0; //Tracks the time stamp of last packet received
 
+float frequency = 910; //Broadcast frequency
 
 void setup() {
   SerialUSB.begin(9600);
@@ -39,43 +42,41 @@ void loop() {
 }
 
 void startTimer(int frequencyHz) {
-  configureClock();
-  configureTimer(frequencyHz);
+  configureClockTC4();
+  configureTimerTC4(frequencyHz);
 }
-
-void configureClock() {
-  REG_GCLK_CLKCTRL = (uint16_t)(GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_ID_TCC2_TC3);
+void configureClockTC4() {
+  REG_GCLK_CLKCTRL = (uint16_t)(GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_ID_TC4_TC5);
   while (GCLK->STATUS.bit.SYNCBUSY == 1);
 }
 
-void configureTimer(int frequencyHz) {
-  TcCount16* TC = (TcCount16*)TC3;
+void configureTimerTC4(int frequencyHz) {
+  TcCount16* TC = (TcCount16*)TC4;
   TC->CTRLA.reg &= ~TC_CTRLA_ENABLE;
   while (TC->STATUS.bit.SYNCBUSY == 1);
 
   TC->CTRLA.reg |= TC_CTRLA_MODE_COUNT16 | TC_CTRLA_WAVEGEN_MFRQ | TC_CTRLA_PRESCALER_DIV1024;
   while (TC->STATUS.bit.SYNCBUSY == 1);
 
-  setTimerFrequency(TC, frequencyHz);
+  setTimerFrequencyTC4(TC, frequencyHz);
   TC->INTENSET.reg = 0;
   TC->INTENSET.bit.MC0 = 1;
 
-  NVIC_EnableIRQ(TC3_IRQn);
+  NVIC_EnableIRQ(TC4_IRQn);
   TC->CTRLA.reg |= TC_CTRLA_ENABLE;
   while (TC->STATUS.bit.SYNCBUSY == 1);
 }
 
-void setTimerFrequency(TcCount16* TC, int frequencyHz) {
+void setTimerFrequencyTC4(TcCount16* TC, int frequencyHz) {
   int compareValue = (CPU_HZ / (TIMER_PRESCALER_DIV * frequencyHz)) - 1;
   TC->CC[0].reg = compareValue;
   while (TC->STATUS.bit.SYNCBUSY == 1);
 }
 
-void TC3_Handler() {
+void TC4_Handler() {
   static bool isLEDOn = false;
-  TcCount16* TC = (TcCount16*)TC3;
-  if (TC->INTFLAG.bit.MC0 == 1) 
-  {
+  TcCount16* TC = (TcCount16*)TC4;
+  if (TC->INTFLAG.bit.MC0 == 1) {
     TC->INTFLAG.bit.MC0 = 1;
     digitalWrite(PIN_LED_13, isLEDOn);
     SerialUSB.println(isLEDOn ? "Blue LED is on" : "Blue LED is off");
