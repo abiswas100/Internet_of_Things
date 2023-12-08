4cd4f2d72d9e658c911b228921f2b9a1af02ce1a
Avhishek <abiswas3@huskers.unl.edu>
2023-12-07 19:03:30 -0600

    added code to connect to cloud and send data periodically

--- Course_Project/readfile_send_data.py
+++ Course_Project/readfile_send_data.py
@@ -1,75 +1,53 @@
-
-
 import random
 import time
-
 from iotc.models import Command, Property
 from iotc import IoTCClient, IOTCConnectType, IOTCEvents
-
 import keys
 
-import random
-import time
-
-from iotc.models import Command, Property
-from iotc import IoTCClient, IOTCConnectType, IOTCEvents
-
-# scope_id = '0ne00B6D4A2'
-# device_id = 'scv5qwawrz'
-# device_key = 'xdtpUXfIqjTO9St6mlweOCa2Gx9qFawjkfMjgckvyi4='
-
-def send_to_cloud(temperature, humidity, light_intensity, soil_moisture, door_state, light_state, fan_state, pump_state):
-    def on_commands(command: Command):
-        print(f"{command.name} command was sent")
-        command.reply()
-
-    iotc = IoTCClient(
-        keys.device_id,
-        keys.scope_id,
-            IOTCConnectType.IOTC_CONNECT_DEVICE_KEY,
-            keys.device_key)
-
-    iotc.connect()
-
-    iotc.on(IOTCEvents.IOTC_COMMAND, on_commands)
-
-    # iotc.send_property({
-    #     "LastTurnedOn": time.time()
-    # })
-
-    # while iotc.is_connected():
-
+def send_to_cloud(iotc, temperature, humidity, light_intensity, soil_moisture, door_state, light_state, fan_state, pump_state):
     if iotc.is_connected():
         iotc.send_telemetry({
             'temperature': temperature,
-            'humidity' : humidity,
-            'light_intensity' : light_intensity,
-            'soil_moisture' : soil_moisture,
+            'humidity': humidity,
+            'light_intensity': light_intensity,
+            'soil_moisture': soil_moisture,
             "door_state": door_state,
-            "light_state" : light_state,
-            "fan_state" :fan_state,
-            "pump_state" : pump_state
+            "light_state": light_state,
+            "fan_state": fan_state,
+            "pump_state": pump_state
         })
-        iotc.disconnect()
-        # time.sleep(10)
-    return 1
+        
 
+def on_commands(command: Command):
+    print(f"{command.name} command was sent")
+    command.reply()
 
 if __name__ == "__main__":
-    
-    temperature, humidity, light_intensity, soil_moisture = 0.0, 0.0, 0.0, 0.0
-    door_state, light_state, fan_state, pump_state = 0,0,0,0
+    # Initialize and connect the IoT client
+    iotc = IoTCClient(
+        keys.device_id,
+        keys.scope_id,
+        IOTCConnectType.IOTC_CONNECT_DEVICE_KEY,
+        keys.device_key)
 
+    iotc.connect()
+    iotc.on(IOTCEvents.IOTC_COMMAND, on_commands)
+
+    # Initialize sensor data variables
+    temperature, humidity, light_intensity, soil_moisture = 0.0, 0.0, 0.0, 0.0
+    door_state, light_state, fan_state, pump_state = 0, 0, 0, 0
+
+    # Read and process the sensor data file
     with open('sensor_data8.txt', 'r') as file:
         for line in file:
             if "DHT_Temperature" in line:
-                    temperature = line.split('=')[-1].strip()
+                temperature = float(line.split('=')[-1].strip())
             elif "Humidity" in line:
-                humidity = line.split('=')[-1].strip()
+                humidity = float(line.split('=')[-1].strip())
             elif "Light intensity" in line:
-                light_intensity = line.split('=')[-1].strip()
+                light_intensity = float(line.split('=')[-1].strip())
             elif "Soil Moisture" in line:
-                soil_moisture = line.split('=')[-1].strip()
+                soil_moisture = float(line.split('=')[-1].strip())
             elif "Door State" in line:
                 door_state = 0 if "Closed" in line else 1
             elif "LIGHTS" in line:
@@ -78,9 +56,96 @@ if __name__ == "__main__":
                 fan_state = 1 if "ON" in line else 0
             elif "PUMP" in line:
                 pump_state = 1 if "ON" in line else 0
+
+            # Send the parsed data to the cloud
+            send_to_cloud(iotc, temperature, humidity, light_intensity, soil_moisture, door_state, light_state, fan_state, pump_state)
+            time.sleep(2)
+
+    # Disconnect after all data has been sent
+    iotc.disconnect()
+
+
+# import random
+# import time
+
+# from iotc.models import Command, Property
+# from iotc import IoTCClient, IOTCConnectType, IOTCEvents
+
+# import keys
+
+# import random
+# import time
+
+# from iotc.models import Command, Property
+# from iotc import IoTCClient, IOTCConnectType, IOTCEvents
+
+# # scope_id = '0ne00B6D4A2'
+# # device_id = 'scv5qwawrz'
+# # device_key = 'xdtpUXfIqjTO9St6mlweOCa2Gx9qFawjkfMjgckvyi4='
+
+# def send_to_cloud(temperature, humidity, light_intensity, soil_moisture, door_state, light_state, fan_state, pump_state):
+#     def on_commands(command: Command):
+#         print(f"{command.name} command was sent")
+#         command.reply()
+
+#     iotc = IoTCClient(
+#         keys.device_id,
+#         keys.scope_id,
+#             IOTCConnectType.IOTC_CONNECT_DEVICE_KEY,
+#             keys.device_key)
+
+#     iotc.connect()
+
+#     iotc.on(IOTCEvents.IOTC_COMMAND, on_commands)
+
+#     # iotc.send_property({
+#     #     "LastTurnedOn": time.time()
+#     # })
+
+#     # while iotc.is_connected():
+
+#     if iotc.is_connected():
+#         iotc.send_telemetry({
+#             'temperature': temperature,
+#             'humidity' : humidity,
+#             'light_intensity' : light_intensity,
+#             'soil_moisture' : soil_moisture,
+#             "door_state": door_state,
+#             "light_state" : light_state,
+#             "fan_state" :fan_state,
+#             "pump_state" : pump_state
+#         })
+#         iotc.disconnect()
+#         # time.sleep(10)
+#     return 1
+
+
+# if __name__ == "__main__":
+    
+#     temperature, humidity, light_intensity, soil_moisture = 0.0, 0.0, 0.0, 0.0
+#     door_state, light_state, fan_state, pump_state = 0,0,0,0
+
+#     with open('sensor_data8.txt', 'r') as file:
+#         for line in file:
+#             if "DHT_Temperature" in line:
+#                     temperature = line.split('=')[-1].strip()
+#             elif "Humidity" in line:
+#                 humidity = line.split('=')[-1].strip()
+#             elif "Light intensity" in line:
+#                 light_intensity = line.split('=')[-1].strip()
+#             elif "Soil Moisture" in line:
+#                 soil_moisture = line.split('=')[-1].strip()
+#             elif "Door State" in line:
+#                 door_state = 0 if "Closed" in line else 1
+#             elif "LIGHTS" in line:
+#                 light_state = 1 if "ON" in line else 0
+#             elif "FANS ON" in line:
+#                 fan_state = 1 if "ON" in line else 0
+#             elif "PUMP" in line:
+#                 pump_state = 1 if "ON" in line else 0
                 
-            print(temperature, humidity, light_intensity, soil_moisture, door_state, light_state, fan_state, pump_state)
-            print("here")
-            returned = send_to_cloud(temperature, humidity, light_intensity, soil_moisture, door_state, light_state, fan_state, pump_state)
+#             print(temperature, humidity, light_intensity, soil_moisture, door_state, light_state, fan_state, pump_state)
+#             print("here")
+#             returned = send_to_cloud(temperature, humidity, light_intensity, soil_moisture, door_state, light_state, fan_state, pump_state)
             
             
